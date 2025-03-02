3a70284070e19ca179178aeabbde6b94b9f8e20c
lgreg1908 <l.gregoretti@gmail.com>
2025-03-02 16:07:18 -0500

    test for parse_percentage

---
+++ pipeline/tests/clean/utils/test_parse_percentage.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import pytest
+
+from pipeline.src.clean.utils import parse_percentage
+
+def test_parse_percentage_vectorized():
+    """
+    Test conversion on a Series containing multiple percentage strings.
+    """
+    inputs = ["14%", "0%", "100%", "---", "42%", "", " 7%", "abc%"]
+    expected = [14.0, 0.0, 100.0, np.nan, 42.0, np.nan, 7.0, np.nan]
+    series = pd.Series(inputs)
+    result = parse_percentage(series)
+    
+    for idx, (res, exp_val) in enumerate(zip(result, expected)):
+        if pd.isna(exp_val):
+            assert pd.isna(res), f"At index {idx}, expected NaN for input '{inputs[idx]}' but got {res}"
+        else:
+            assert res == pytest.approx(exp_val), (
+                f"At index {idx}, expected {exp_val} for input '{inputs[idx]}' but got {res}"
+            )
